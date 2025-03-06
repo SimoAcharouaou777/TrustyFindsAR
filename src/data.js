525db090ebcb646d63450c12a5dba7ca18dea4ea
MOHAMED ACHAROUAOU <acharouaoumohamed@gmail.com>
2025-03-06 23:55:39 +0000

    added products

--- src/data.js
+++ src/data.js
@@ -2,33 +2,27 @@
 export const products = [
   {
     id: 1,
-    name: 'عباية أنيقة بتصميم عصري',
-    price: 49.61,
-    category: 'fashion',
-    rating: 4.7,
-    reviews: 224,
-    image: '//ae01.alicdn.com/kf/S437363f15ee8461fa4bec2cb5bdb1cbdI.jpg_350x350.jpg',
-    affiliateLink: 'https://s.click.aliexpress.com/e/_oo4UAcm',
-    // يمكنك إضافة المزيد من الخصائص هنا إن أردت
+    name: "عباية كيمونو قفطان دبي فستان إسلامي عبايات قفطان ماروكين قطر عمان تركيا ملابس للنساء رداء نسائي",
+    price: 49.30,
+    originalPrice: 61.62,
+    discountPercentage: 20,
+    rating: 5,
+    reviews: 39,
+    image: "//ae01.alicdn.com/kf/S437363f15ee8461fa4bec2cb5bdb1cbdI.jpg_350x350.jpg",
+    affiliateLink: "https://s.click.aliexpress.com/e/_oCO5s0Q",
+    category: "fashion"
   },
   {
     id: 2,
-    name: 'سماعات بلوتوث لاسلكية',
-    price: 129.00,
-    category: 'electronics',
-    rating: 4.8,
-    reviews: 315,
-    image: '//ae01.alicdn.com/kf/S437363f15ee8461fa4bec2cb5bdb1cbdI.jpg_350x350.jpg',
-    affiliateLink: 'https://s.click.aliexpress.com/e/_dU2t0Qm',
+    name: "عيد مسلم لؤلؤة عباية للنساء الديكور رمضان سترة مطوي فستان طويل المغرب قفطان عبايات طويلة دبي رداء جلابية 2024",
+    price: 49.61,
+    originalPrice: 130.90,
+    discountPercentage: 62,
+    rating: 4.6,
+    reviews: 109,
+    image: "//ae01.alicdn.com/kf/S437363f15ee8461fa4bec2cb5bdb1cbdI.jpg_350x350.jpg",
+    affiliateLink: "https://s.click.aliexpress.com/e/_oF2wfgw",
+    category: "fashion"
   },
-  {
-    id: 3,
-    name: 'مجموعة مكياج كاملة',
-    price: 99.00,
-    category: 'makeup',
-    rating: 4.5,
-    reviews: 89,
-    image: '//ae01.alicdn.com/kf/S437363f15ee8461fa4bec2cb5bdb1cbdI.jpg_350x350.jpg',
-    affiliateLink: 'https://s.click.aliexpress.com/e/_dTg3FGG',
-  },
+
 ];
--- src/homeProducts.js
+++ src/homeProducts.js
@@ -34,5 +34,16 @@ export const homeProducts = [
         image: "//ae01.alicdn.com/kf/S3a9f11df82974b38ab262e552fc67e8fq.jpg_350x350.jpg",
         link: "https://s.click.aliexpress.com/e/_ondSoiE",
     },
+    {
+        id: 4,
+        name: "مياكي مطوي طباعة طوي طوق بأكمام طويلة خندق معطف للنساء 2024 شتاء جديد الأصلي مصمم موضة سترة فستان",
+        price: 126.81,
+        originalPrice: 265.95,
+        discountPercentage: 52,
+        rating: 4.4,
+        reviews: 192,
+        image: "//ae01.alicdn.com/kf/S9f2e2b768dfe4edba3ed456a71f2d9352.jpg_350x350.jpg",
+        link: "https://s.click.aliexpress.com/e/_onfs866",
+    }
     // أضف المزيد من المنتجات حسب رغبتك
 ];
