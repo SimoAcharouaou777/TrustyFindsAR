b64e7f3ad84a9bcdde3feb1bbdb9be42797d95f4
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2025-05-03 16:26:08 +0100

    Add new product details for Ninja Swirl Ice Cream Maker

--- src/homeProducts.js
+++ src/homeProducts.js
@@ -12,5 +12,16 @@ export const homeProducts = [
         image: "https://a.media-amazon.com/images/I/51GbD-P5LlL._AC_SX679_.jpg", 
         link: "https://amzn.to/42TzRZn",
     },
+    {
+        id: 2,
+        name: "Ninja Swirl by CREAMi Ice Cream and Soft Serve Maker",
+        price: 300.38,
+        originalPrice: 399.99,
+        discountPercentage: 25,
+        rating: 4.4,
+        reviews: 230,
+        image: "https://a.media-amazon.com/images/I/810HM-npDOL._AC_SX679_.jpg", 
+        link: "https://amzn.to/43dZe9s",
+    },
     
 ];
